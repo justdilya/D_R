ebb37db41142001af8dd5344c930e08aba9430c6
Диля <dilnozarahmova003@gmail.com>
2025-10-07 13:26:33 +0300

    My first node in ROS, AOAOAO!

--- scripts/first_node.py
+++ scripts/first_node.py
@@ -1,17 +1,5 @@
-#!/usr/bin/env python3
-
-import rospy
-
 def main():
-    # Инициализируем узел ROS с именем "first_node"
-    rospy.init_node('first_node')
-    
-    # Выводим сообщение
-    rospy.loginfo("Hello ROS World!")
-    
-    # Чтобы программа не завершалась сразу
-    rospy.spin()
-
+print(f"Hello ROS World!")
 if __name__ == "__main__":
-    main()
+main()
 
