f56440cf052ae919d163cd3d031c5aab51b12727
justdilya <dilnozarahmova003@gmail.com>
2025-10-20 20:51:43 +0300

    Update time_node.py

--- scripts/time_node.py
+++ scripts/time_node.py
@@ -10,7 +10,7 @@ def main():
     while not rospy.is_shutdown():
         now = datetime.datetime.now()
         current_time = now.strftime("%H:%M:%S")
-        rospy.loginfo(f"Time: {current_time}")
+        print(f"Time: {current_time}")
         time.sleep(5)
 
 if __name__ == "__main__":
